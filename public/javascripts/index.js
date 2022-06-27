35a5d8e2663bf90190a5150a27845a05d8cc2b12
Douglas Bewiahn <dwbewiahn@gmail.com>
2022-06-27 11:50:18 +0100

    Login and CSS
    Login Page and basic CSS theme created

---
+++ public/javascripts/index.js
@@ -0,0 +1,10 @@
+async function login() {
+    let user = document.getElementById("user").value;
+    let password = document.getElementById("password").value;
+
+    if (user.length == 0 || password.length ==0) {
+        document.getElementById("errorLogin").innerHTML = "Inform User/Password to login";
+        return;
+    }
+
+}
